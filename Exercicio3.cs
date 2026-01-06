337eebc453e40b5ae7b62984280dded93023efed
Miratii <emersuncampos@outlook.com>
2026-01-05 22:47:33 -0300

    feat: implementa lógica de verificação de aprovação estudantil

---
+++ Exercicio3.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("---Verificação de Aprovação Estudantil---\t");
+
+Console.WriteLine("\nInsira a nota do(a) aluno(a):");
+
+var aluno = Convert.ToDouble(Console.ReadLine());
+
+if (aluno < 5 )
+{
+    Console.WriteLine("Aluno reprovado!");
+}
+
+else if (aluno >= 5 && aluno > 6.9)
+{
+    Console.WriteLine("Aluno em recuperação!");
+}
+else
+{
+    Console.WriteLine("Aluno aprovado");    
+}
