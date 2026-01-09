f9af475f74c7f7d93b550dda4c8cd6fcfc5fc16e
Miratii <emersuncampos@outlook.com>
2026-01-09 20:13:46 -0300

    feat: verifica paridade numérica usando operador de módulo e switch

---
+++ Exercicio5.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("##---Verificação de impar ou par---##\t");
+
+Console.WriteLine("\nDigite um número");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num % 2)
+{
+
+    case 0:
+        Console.WriteLine($"O {num} é PAR");
+        break;
+    case 1:
+        Console.WriteLine($"O {num} é IMPAR");
+        break;
+
+}
+
+
