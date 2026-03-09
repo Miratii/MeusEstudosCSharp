6684cf1ea762e50c833963264218649e4c682cac
Miratii <emersuncampos@outlook.com>
2026-03-09 00:27:57 -0300

    Feat: Implementação inicial do Portal do Aluno com Menu e Logs

---
+++ Projeto_Portal_Aluno.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("#---Portal do Aluno---#\n");
+
+Console.WriteLine("Escolha a opção desejada:\n 1. Verificar aprovação \n 2.Gerador de Tabuada de Estudos\n 3. Gerar mapa de Coordenadas\n 0. Sair\n");
+
+int opcao = Convert.ToInt32(Console.ReadLine());
+double nota;
+while (opcao != 0)
+{
+
+
+switch (opcao)
+    {
+    case 1:
+        Console.WriteLine("Bem vindo (a) ao portal de notas! Por favor digite sua nota:");
+        
+        nota = Convert.ToDouble(Console.ReadLine());
+
+        if (nota >= 6)
+            Console.WriteLine("Você foi aprovado! Parabéns! Pressione 0 para sair:");
+        else
+        {
+            Console.WriteLine("Você foi está de recuperação. Converse com a coordenação para mais informações.\nPressione 0 para sair:");
+
+            
+        }
+            if (opcao == 0)
+            {
+                break;
+            }
+            break;
+    }
+}
+
+
+
+
+
+
