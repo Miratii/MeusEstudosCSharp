cf6fb59130fcd8d2f3cc33678f271de367b2a536
Miratii <emersuncampos@outlook.com>
2026-03-12 14:15:49 -0300

    feat: ajuste no fluxo do menu e implementação de feedback visual de notas

--- Projeto_Portal_Aluno.cs
+++ Projeto_Portal_Aluno.cs
@@ -1,36 +1,42 @@
-﻿Console.WriteLine("#---Portal do Aluno---#\n");
-
-Console.WriteLine("Escolha a opção desejada:\n 1. Verificar aprovação \n 2.Gerador de Tabuada de Estudos\n 3. Gerar mapa de Coordenadas\n 0. Sair\n");
-
-int opcao = Convert.ToInt32(Console.ReadLine());
-double nota;
-while (opcao != 0)
+﻿int menu = -1;
+while (menu != 0)
 {
+    Console.WriteLine("#---Portal do Aluno---#\n");
+
+    Console.WriteLine("Escolha a opção desejada:\n 1. Ver/Calcular minha média de notas \n 2. Matricular em uma disciplina\n 3. Visualizar meu horário de aulas\n 0. Sair\n");
+
+    menu = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    double nota;
 
 
-switch (opcao)
+
+    switch (opcao)
     {
-    case 1:
-        Console.WriteLine("Bem vindo (a) ao portal de notas! Por favor digite sua nota:");
-        
-        nota = Convert.ToDouble(Console.ReadLine());
+        case 1:
+            Console.WriteLine("Bem vindo (a) ao portal de notas! Por favor digite sua nota:");
 
-        if (nota >= 6)
-            Console.WriteLine("Você foi aprovado! Parabéns! Pressione 0 para sair:");
-        else
-        {
-            Console.WriteLine("Você foi está de recuperação. Converse com a coordenação para mais informações.\nPressione 0 para sair:");
+            nota = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
 
-            
-        }
-            if (opcao == 0)
+            if (nota >= 6)
+                Console.WriteLine("Você foi aprovado! Parabéns!\n");
+            else
             {
-                break;
+                Console.WriteLine("Você foi reprovado.\nPressione 0 para sair:");
+
+
             }
+
             break;
     }
+    if (menu != 0)
+    {
+        Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+        Console.ReadKey();
+        
+    }
 }
-
 
 
 
