a93289768b2584f5354256d03652907968eeb187
Miratii <emersuncampos@outlook.com>
2026-03-12 21:13:25 -0300

    feat: adiciona exercício de tabuada interativa com laço for

---
+++ Exercicio12.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Instrução for");
+int num, resultado;
+
+Console.WriteLine("\nDigite um número que deseja visualizar na tabuada");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    resultado = num * i;
+    Console.WriteLine($" {num} X {i} = {resultado}");
+}
+
