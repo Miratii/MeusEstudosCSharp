b3dece4668667bd1dd1ffb3a9448cdc132d2b115
Miratii <emersuncampos@outlook.com>
2026-04-02 13:24:05 -0300

    feat: implementa herança e polimorfismo com exemplo de Funcionários

---
+++ Funcionario3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pratica
+{
+    class Funcionário3 : Funcionario
+    {
+        public int Idade { get; set; }
+
+        public override double ParticipacaoLucro()
+        {
+            return base.ParticipacaoLucro() * 0.3;
+        }
+    }
+}
